beeb3f467e8df0cac5914ddd60664cd251f298b8
MarieStLaurent <marie.stl@gmail.com>
2021-04-28 01:19:20 -0400

    fixed disto CIFAR val range

--- data/distortion.py
+++ data/distortion.py
@@ -8,8 +8,6 @@ def distortion(args):
 
     if args.distortion=='gaussian_blur':
         return lambda img: gaussian_blur(img, args.severity, args.experiment=='CIFAR100')
-    elif args.distortion=='speckle_noise':
-        return lambda img: speckle_noise(img, args.severity, args.experiment=='CIFAR100')
     elif args.distortion=='shot_noise':
         return lambda img: shot_noise(img, args.severity, args.experiment=='CIFAR100')
     elif args.distortion=='hyper':
@@ -37,24 +35,6 @@ def gaussian_blur(img, severity=2, cifar=False):
     return x
 
 
-def speckle_noise(img, severity=4, cifar=False):
-
-    c = [5, 4, 3, 2, 1][severity - 1]
-    x = np.array(img).astype(np.float32)
-    if cifar:
-        minim = np.min(x)
-        maxi = np.max(x)
-        if maxi == minim:
-            return x - x
-        x = (x - minim) / (maxi-minim)
-    x *= (3 ** c - 1)
-    x = x.round()
-    x /= (2 ** c - 1)
-    x = ((x*(maxi-minim)) + minim) if cifar else x
-
-    return x
-
-
 def shot_noise(img, severity=3, cifar=False):
     # c = [4, 3, 2, 1, 1][2]
     c = [4, 3, 2, 1, 1][severity-1]
@@ -75,50 +55,26 @@ def shot_noise(img, severity=3, cifar=False):
 
 def hyper_distortions(img, cifar=False):
     x = np.array(img).astype(np.float32)
-    if cifar:
-        minim = np.min(x)
-        maxi = np.max(x)
-        if maxi == minim:
-            return x - x
 
-    x = gaussian_blur(x, 3, False)
-    x = speckle_noise(x, 4, False)
-    x = shot_noise(x, 3, False)
-
-    x = ((x*(maxi-minim)) + minim) if cifar else x
+    x = shot_noise(x, 3, cifar)
+    x = gaussian_blur(x, 3, cifar)
 
     return x
 
 
 def medium_distortions(img, cifar=False):
     x = np.array(img).astype(np.float32)
-    if cifar:
-        minim = np.min(x)
-        maxi = np.max(x)
-        if maxi == minim:
-            return x - x
 
-    x = gaussian_blur(x, 2, False)
-    x = speckle_noise(x, 2, False)
-    x = shot_noise(x, 2, False)
-
-    x = ((x*(maxi-minim)) + minim) if cifar else x
+    x = shot_noise(x, 2, cifar)
+    x = gaussian_blur(x, 2, cifar)
 
     return x
 
 
 def fine_distortions(img, cifar=False):
     x = np.array(img).astype(np.float32)
-    if cifar:
-        minim = np.min(x)
-        maxi = np.max(x)
-        if maxi == minim:
-            return x - x
 
-    x = gaussian_blur(x, 1, False)
-    x = speckle_noise(x, 1, False)
-    x = shot_noise(x, 1, False)
-
-    x = ((x*(maxi-minim)) + minim) if cifar else x
+    x = shot_noise(x, 1, cifar)
+    x = gaussian_blur(x, 1, cifar)
 
     return x
